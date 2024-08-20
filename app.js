7ecb6be0304d08b586bac290f049b3a8cad41731
Harshada07003 <moryeharshada07@gmail.com>
2024-08-20 11:53:59 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,36 @@
+
+ async function getWeather(city) {
+    if (!city) {
+        city = 'Ratnagiri';
+    }
+
+    const apiKey = "f78aac73c159f87060a914a656d2c09d";
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        if (response.ok) {
+            const iconCode = data.weather[0].icon;
+            const iconUrl = `https://openweathermap.org/img/wn/${iconCode}@2x.png`;
+
+            document.getElementById('weatherInfo').innerHTML = `
+                <img src="${iconUrl}" alt="Weather icon">
+                <h2>Weather in ${data.name}</h2>
+                <h3>${data.main.temp}°C</h3>
+                <h4>${data.weather[0].description}</h4>
+                <p>Humidity:<br>${data.main.humidity}%</p>
+                <p>Wind Speed:<br> ${data.wind.speed} m/s</p>`;
+        } else {
+            document.getElementById('weatherInfo').innerHTML = `<p>${data.message}</p>`;
+        }
+    } catch (error) {
+        console.error("Error fetching weather data:", error);
+        document.getElementById('weatherInfo').innerHTML = `<p>Error fetching data. Please try again.</p>`;
+    }
+}
+
+window.onload = () => {
+    getWeather();
+};
